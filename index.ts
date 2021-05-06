aa6575899c625671356a0c0224f94947cded311b
Harold Mondragon <haroldpmt@hotmail.com>
2021-05-06 16:37:56 -0500

    Primer commit - rest server

---
+++ index.ts
@@ -0,0 +1,23 @@
+import Server from "./classes/server";
+import router from "./routes/router";
+import express from 'express';
+import cors from 'cors';
+
+const server = new Server();
+
+
+server.app.use( express.urlencoded( { extended: true }));
+server.app.use( express.json());
+
+
+// CORS
+server.app.use( cors({ origin: true, credentials: true }));
+
+
+// Rutas de Servicio
+server.app.use('/', router);
+
+
+server.start( () => {
+    console.log(`Servidor corrienfo en el puerto ${ server.port }`)
+});
